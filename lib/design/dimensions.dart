2ecb256ad17d047b923c3e117b115071592cbe4c
ginter <127141517+ginterloper@users.noreply.github.com>
2025-05-23 05:50:59 +0300

    added links to dimension (#22)

--- lib/design/dimensions.dart
+++ lib/design/dimensions.dart
@@ -77,3 +77,6 @@ const double fontSize15 = 15;
 const double fontSize17 = 17;
 const double fontSize24 = 24;
 const double fontSize40 = 40;
+
+const String connectionTgUrl = 't.me/vpn_client_bot';
+const String supportTgUrl = 't.me/vpn_client_support';
